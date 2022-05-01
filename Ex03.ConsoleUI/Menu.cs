fe3254b5a1aa601e13d3d4e4cd783197bca80dc1
Stav Shay <stavsy@mta.ac.il>
2022-05-01 20:42:13 +0300

    Adeed detalis to menu not sure how to run the main

--- Ex03.ConsoleUI/Menu.cs
+++ Ex03.ConsoleUI/Menu.cs
@@ -1,6 +1,130 @@
 ﻿namespace Ex03.ConsoleUI
 {
+    using System;
     public class Menu
     {
+       
+        public void RunMenu()
+        {
+            eMenuOption userChoice = eMenuOption.Start;
+            Console.WriteLine("Hello! Welcome to the GARAGE!");
+            do
+            {
+                PrintMenu();
+                try
+                {
+                    GetChoice(ref userChoice);
+                    ExecuteUserChoice(userChoice);
+                }
+                catch (FormatException ex)
+                {
+                    // invalid input 
+                }
+                catch (ArgumentException ex)
+                {
+                    // invalid input in logic means
+                }
+                catch(Ex03.GarageLogic.ValueOutOfRangeException ex)
+                {
+                    // value out of range
+                }
+
+            } while (userChoice != eMenuOption.Exit);
+        }
+
+        private void PrintMenu()
+        {
+            Console.WriteLine(@"Please enter your choice:
+1. Enter new vehicle
+2. Show the garage's vehicle license numbers list.
+3. Change vehicle state.
+4. Infale air in the vehicle wheels.
+5. Fuel a regular vehicle.
+6. Charge an elecric vehicle.
+7. Show vehicle's full details.
+8. Exit");
+        }
+
+        private void GetChoice(ref eMenuOption io_UserChoice)
+        {
+           
+            bool legalInput = int.TryParse(Console.ReadLine(), out int userChoice);
+            if(!legalInput)
+            {
+                throw new FormatException();
+            }
+            if(userChoice <= 0 || userChoice > 8)
+            {
+                throw new Ex03.GarageLogic.ValueOutOfRangeException("Your choice isn't one of the options", 1, 8);
+            }
+            io_UserChoice = (eMenuOption)userChoice;
+
+        }
+
+        private void ExecuteUserChoice(eMenuOption i_UserChoice)
+        {
+            if(i_UserChoice == eMenuOption.LicenseNumbersList)
+            {
+                //PrintList
+            }
+            else
+            {
+                AskUserLisencePlateNumber(out string lisencePlateNumber);
+                switch (i_UserChoice)
+                {
+                    case eMenuOption.NewVehicle:
+                        CheckIfVehicleExist(lisencePlateNumber);
+                        break;
+                    case eMenuOption.ChangeVehicleState:
+                        break;
+                    case eMenuOption.InflationWheelAirToMax:
+                        break;
+                    case eMenuOption.FuelVehicle:
+                        break;
+                    case eMenuOption.ChargingVehicle:
+                        break;
+                    case eMenuOption.FullDetailsOnVehicle:
+                        break;
+                }
+           
+
+            }
+
+            
+            
+        }
+        
+        private void CheckIfVehicleExist(string i_LisencePlateNumber)
+        {
+            if (IsLisencePlateNumberExist(i_LisencePlateNumber)) // LogicMethod
+            {
+                Console.WriteLine("This vehicle has already in the garage!");
+                //change the vehicle state - logic method
+            }
+            else
+            {
+                
+                
+                //AddNewVehicle(i_LisencePlateNumber); - LogicMethod
+            }
+        }
+        private  void AskUserLisencePlateNumber(out string o_LisencePlateNumber)
+        {
+            Console.WriteLine("Please enter your lisence plate number: ");
+            o_LisencePlateNumber = Console.ReadLine();
+            if(o_LisencePlateNumber.Length == 0)
+            {
+                throw new FormatException();
+            }
+        }
+
+   
+
+    
+
+
+
+
+
     }
 }
---
+++ Ex03.ConsoleUI/eMenuOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    public enum eMenuOption
+    {
+        Start,
+        NewVehicle,
+        LicenseNumbersList,
+        ChangeVehicleState,
+        InflationWheelAirToMax,
+        FuelVehicle,
+        ChargingVehicle,
+        FullDetailsOnVehicle,
+        Exit
+    }
+}
