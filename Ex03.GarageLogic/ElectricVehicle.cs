193115ecdf669aef21348990f95304e4c440c658
Oshri Fadlon <oshrifa@mta.ac.il>
2022-05-01 16:59:24 +0300

    add get set for the current data members we have now

--- Ex03.GarageLogic/ElectricVehicle.cs
+++ Ex03.GarageLogic/ElectricVehicle.cs
@@ -5,6 +5,32 @@
         private float m_BatteryTimeLeft;
         private float m_MaxBatteryTime;
 
+        float BatteryTimeLeft
+        {
+            get
+            {
+                return m_BatteryTimeLeft;
+            }
+
+            set
+            {
+                m_BatteryTimeLeft = value;
+            }
+        }
+
+        public float MaxBatteryTime
+        {
+            get
+            {
+                return m_MaxBatteryTime;
+            }
+
+            set
+            {
+                m_MaxBatteryTime = value;
+            }
+        }
+
         public void ChargeTheBattery(float i_AmountOfTimeToCharge)
         {
             if (m_MaxBatteryTime >= m_BatteryTimeLeft + i_AmountOfTimeToCharge)
--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
 
+    // to do get wheel and get all the wheels
     public class Vehicle
     {
        private string m_ModelName;
@@ -10,5 +11,57 @@
        private float m_PrecentEnergy;
        private List<Wheel> m_CollectionOfWheels;
        private Customer m_Owner;
+
+       public string ModelName
+       {
+            get
+            {
+                return m_ModelName;
+            }
+
+            set
+            {
+                m_ModelName = value;
+            }
+       }
+
+       public string LicensePlateNumber
+        {
+            get
+            {
+                return m_LicensePlateNumber;
+            }
+
+            set
+            {
+                m_LicensePlateNumber = value;
+            }
+       }
+
+       public float PrecentEnergy
+       {
+            get
+            {
+                return m_PrecentEnergy;
+            }
+
+            set
+            {
+                m_PrecentEnergy = value;
+            }
+       }
+
+       public Customer Owner
+        {
+            get
+            {
+                return m_Owner;
+            }
+
+            set
+            {
+                m_Owner = value;
+            }
+        }
     }
 }
