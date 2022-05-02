ffe58346d9b3c1501320ef3d019e007d96e4afe1
Oshri Fadlon <oshrifa@mta.ac.il>
2022-05-02 16:38:00 +0300

    class value out of range

--- Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -7,6 +7,13 @@
         private float m_MaxValue;
         private float m_MinValue;
 
+        public ValueOutOfRangeException(float i_MaxValue, float i_MinValue)
+            : base(string.Format(@"Values out of range, Expected values are {0} and {1}", i_MinValue, i_MaxValue))
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+        }
+
         public float MaxValue
         {
             get
